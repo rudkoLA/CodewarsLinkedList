a07463e40a324ad73de4a5caa9b3dce2619fe33d
rudkoLA <rudko.adam.1@gmail.com>
2025-03-26 11:25:26 +0200

    second attempt at fourth challenge

--- Linked-Lists-Get-Nth-Node.py
+++ Linked-Lists-Get-Nth-Node.py
@@ -1,6 +1,9 @@
 from preloaded import Node
 
 def get_nth(node, index):
+    if index < 0:
+        raise IndexError
+
     for _ in range(index):
         if node is None:
             raise IndexError
