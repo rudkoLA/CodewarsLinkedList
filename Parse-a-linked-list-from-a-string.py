b45c070488695460626618f06ce2c6f12188af1e
rudkoLA <rudko.adam.1@gmail.com>
2025-03-26 11:09:21 +0200

    success at second challenge

--- Parse-a-linked-list-from-a-string.py
+++ Parse-a-linked-list-from-a-string.py
@@ -5,7 +5,7 @@ def linked_list_from_string(s):
         return None
 
     data.pop(0)
-    data = map(lambda x: int(x) if x.isnumeric() else x, data)
+    data = map(lambda x: int(x) if x.strip().isnumeric() else x, data)
     head = None
 
     for item in data:
