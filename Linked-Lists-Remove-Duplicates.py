f69583bc923bb5260f5ba366cf4aaa6610e56465
rudkoLA <rudko.adam.1@gmail.com>
2025-04-03 17:07:00 +0300

    1st try at 8th challenge

---
+++ Linked-Lists-Remove-Duplicates.py
@@ -0,0 +1,18 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def remove_duplicates(head):
+    if head is None:
+        return None
+
+    prev = head
+    current = head.next
+
+    while prev is not None and current is not None:
+        if prev.data == current.data:
+            prev.next = current.next
+            current = current.next
+
+    return head
