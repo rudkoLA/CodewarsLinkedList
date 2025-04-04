b0806a9751aa19149a34a9ed7fe9b427edd61059
rudkoLA <rudko.adam.1@gmail.com>
2025-04-04 17:26:26 +0300

    1st attempt at 10th challenge

---
+++ Linked-Lists-Alternating-Split/Linked-Lists-Alternating-Split.py
@@ -0,0 +1,39 @@
+class Node(object):
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+    
+class Context(object):
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+    
+def alternating_split(head):
+    first = head
+    second = head.next
+
+    last_first = first
+    last_second = second
+
+    if head.next.next is None:
+        return Context(first, second)
+
+    switch = False
+
+    head = head.next.next
+
+    while head is not None:
+        if switch:
+            last_first.next = head
+            last_first = last_first.next
+        else:
+            last_second.next = head
+            last_second = last_second.next
+
+        head = head.next
+        switch = not switch
+
+    # last_first.next = None
+    # last_second.next = None
+
+    return Context(first, second)
