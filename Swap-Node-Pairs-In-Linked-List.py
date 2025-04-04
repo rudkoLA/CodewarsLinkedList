4c132896a5dc76f88a41024b5518734c622dcb2a
rudkoLA <rudko.adam.1@gmail.com>
2025-04-04 08:53:45 +0300

    1st try at 9th challenge

---
+++ Swap-Node-Pairs-In-Linked-List.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, next=None):
+        self.next = next
+
+def swap_pairs(head):
+    root = head.next
+
+    prev = None
+    current = head
+
+    while current and current.next:
+        rest = current.next.next
+        temp = current.next
+
+        temp.next = current
+        current.next = rest
+
+        prev.next = temp
+
+        prev = current
+        current = rest
+
+    return root
